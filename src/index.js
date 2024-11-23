3bda98a5c49adbdd1ad4ed8cbaf0fabe7c423265
Sachin230498 <sachinpathe230498@gmail.com>
2024-11-23 14:58:11 +0530

    backend

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import Connection from "./config/ConnectDB.js";
-import User from "./model/user.model.js";
 import router from "./routes/user.routes.js";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
