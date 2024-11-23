453a01e6904b124ddfc46b64e6dfd2f7fff12e83
Sachin230498 <sachinpathe230498@gmail.com>
2024-11-23 14:49:29 +0530

    backend

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import Connection from "./config/ConnectDB.js";
+import User from "./model/user.model.js";
+import router from "./routes/user.routes.js";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+
+const app = express();
+
+app.use(bodyParser.json())
+
+Connection(process.env.Mongo_url);
+
+
+app.use("/user", router);
+
+// Server
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
